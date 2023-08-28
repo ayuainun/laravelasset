1f3415aceaf8144063a5984f1ab57084f79a4e71
Ayu Ainun A'ziziyyah <116784668+ayuainun@users.noreply.github.com>
2023-08-28 09:48:56 +0700

    Update show.blade.php
    menambahkan status

--- resources/views/autoproducts/show.blade.php
+++ resources/views/autoproducts/show.blade.php
@@ -30,6 +30,9 @@
 <div class="container-xl px-2 mt-n10">
         <div class="row">
             <div class="col-xl-4">
+                {{-- <div class="col-md-6">
+                    <button class="btn btn-white"><i class="fa fa-print fa-lg"></i><a href="{{ route('autoproducts.pdf', ['id' => $autoproduct->id]) }}" target="_blank">Print PDF</a></button>
+                </div> --}}
                 <!-- autoproduct image card-->
                 <div class="card mb-4 mb-xl-0">
                     <div class="card-header">Automation Image</div>
@@ -74,6 +77,15 @@
                     <div class="card-body">
                         <!-- Form Row -->
                         <div class="row gx-3 mb-3">
+                            <!-- Form Group (product status) -->
+                            <div class="col-md-6"> 
+                                <label class="small mb-1" for="autoproduct_status">Status</label>
+                                @if ($autoproduct->autoproduct_status)
+                                <div class="form-control form-control-solid">{{ $autoproduct->autoproduct_status }}</div>
+                                @else
+                                    <div class="form-control form-control-solid">N/A</div>
+                                @endif
+                            </div>
                             <!-- Form Group (autoproduct ID) -->
                             <div class="col-md-6">
                                 <label class="small mb-1" for="autoproduct_assetID">Old ID</label>
