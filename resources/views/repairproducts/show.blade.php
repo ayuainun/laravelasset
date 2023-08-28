e5c42d0c4a467aa3cb0ed74238954e1e305f9cbb
Ayu Ainun A'ziziyyah <116784668+ayuainun@users.noreply.github.com>
2023-08-28 10:46:44 +0700

    Update show.blade.php
    menambahkan status

--- resources/views/repairproducts/show.blade.php
+++ resources/views/repairproducts/show.blade.php
@@ -30,6 +30,9 @@
 <div class="container-xl px-2 mt-n10">
         <div class="row">
             <div class="col-xl-4">
+                {{-- <div class="col-md-6">
+                    <button class="btn btn-white"><i class="fa fa-print fa-lg"></i><a href="{{ route('repairproducts.pdf', ['id' => $repairproduct->id]) }}" target="_blank">Print PDF</a></button>
+                </div> --}}
                 <!-- repairproduct image card-->
                 <div class="card mb-4 mb-xl-0">
                     <div class="card-header">Valve Image</div>
@@ -74,6 +77,15 @@
                     <div class="card-body">
                         <!-- Form Row -->
                         <div class="row gx-3 mb-3">
+                            <!-- Form Group (product status) -->
+                            <div class="col-md-6"> 
+                                <label class="small mb-1" for="repairproduct_status">Status</label>
+                                @if ($repairproduct->repairproduct_status)
+                                <div class="form-control form-control-solid">{{ $repairproduct->repairproduct_status }}</div>
+                                @else
+                                    <div class="form-control form-control-solid">N/A</div>
+                                @endif
+                            </div>
                             <!-- Form Group (repairproduct ID) -->
                             <div class="col-md-6">
                                 <label class="small mb-1" for="repairproduct_assetID">Old ID</label>
