98d46e440dda3bf2db17b3e57870eb2d0b4fa5de
Ayu Ainun A'ziziyyah <116784668+ayuainun@users.noreply.github.com>
2023-08-28 10:16:50 +0700

    Update show.blade.php
    menambahkan status

--- resources/views/bulkproducts/show.blade.php
+++ resources/views/bulkproducts/show.blade.php
@@ -30,6 +30,9 @@
 <div class="container-xl px-2 mt-n10">
         <div class="row">
             <div class="col-xl-4">
+                {{-- <div class="col-md-6">
+                    <button class="btn btn-white"><i class="fa fa-print fa-lg"></i><a href="{{ route('bulkproducts.pdf', ['id' => $bulkproduct->id]) }}" target="_blank">Print PDF</a></button>
+                </div> --}}
                 <!-- bulkproduct image card-->
                 <div class="card mb-4 mb-xl-0">
                     <div class="card-header">Bulk Material Image</div>
@@ -74,6 +77,15 @@
                     <div class="card-body">
                         <!-- Form Row -->
                         <div class="row gx-3 mb-3">
+                            <!-- Form Group (product status) -->
+                            <div class="col-md-6"> 
+                                <label class="small mb-1" for="bulkproduct_status">Status</label>
+                                @if ($bulkproduct->bulkproduct_status)
+                                <div class="form-control form-control-solid">{{ $bulkproduct->bulkproduct_status }}</div>
+                                @else
+                                    <div class="form-control form-control-solid">N/A</div>
+                                @endif
+                            </div>
                             <!-- Form Group (bulkproduct ID) -->
                             <div class="col-md-6">
                                 <label class="small mb-1" for="bulkproduct_assetID">Old ID</label>
