7bb4d45d9547a331a9a3d8cdd66c74f4b396dc1f
Ayu Ainun A'ziziyyah <116784668+ayuainun@users.noreply.github.com>
2023-08-28 09:21:11 +0700

    Update 2023_08_09_095510_create_products_table.php
    menambahkan status

--- database/migrations/2023_08_09_095510_create_products_table.php
+++ database/migrations/2023_08_09_095510_create_products_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('product_status')->nullable();
             $table->string('product_assetID')->nullable();
             $table->string('product_newassetID')->nullable();
             $table->string('product_equip')->nullable();
