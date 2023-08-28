cb0cd194e40750137998cb571690072728c9eca4
Ayu Ainun A'ziziyyah <116784668+ayuainun@users.noreply.github.com>
2023-08-28 10:31:11 +0700

    Update show.blade.php
    menambahkan status

--- resources/views/insproducts/show.blade.php
+++ resources/views/insproducts/show.blade.php
@@ -30,6 +30,9 @@
 <div class="container-xl px-2 mt-n10">
         <div class="row">
             <div class="col-xl-4">
+                {{-- <div class="col-md-6">
+                    <button class="btn btn-white"><i class="fa fa-print fa-lg"></i><a href="{{ route('insproducts.pdf', ['id' => $insproduct->id]) }}" target="_blank">Print PDF</a></button>
+                </div> --}}
                 <!-- Product image card-->
                 <div class="card mb-4 mb-xl-0">
                     <div class="card-header">Instrument Image</div>
@@ -354,6 +357,15 @@
                             <div class="col-md-6">
                                 <label class="small mb-1">Price Repair</label>
                                 <div class="form-control form-control-solid">{{ $insproduct->insproduct_price  }}</div>
+                            </div>
+                            <!-- Form Group (product status) -->
+                            <div class="col-md-6"> 
+                                <label class="small mb-1" for="insproduct_status">Status</label>
+                                @if ($insproduct->insproduct_status)
+                                <div class="form-control form-control-solid">{{ $insproduct->insproduct_status }}</div>
+                                @else
+                                    <div class="form-control form-control-solid">N/A</div>
+                                @endif
                             </div>
                             <!-- Form Group (remark) -->
                             <div class="col-md-6">
